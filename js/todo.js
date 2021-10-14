63ac674f9b04d53394930c03e65c8b984e5626f8
Joseph <jxlove2020@gmail.com>
2021-10-14 11:22:03 +0900

    localStorage 사용 Loading, Saving, Deleting 적용

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,36 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = document.querySelector('#todo-form input');
 const toDoList = document.getElementById('todo-list');
 
+const TODOS_KEY = 'todos';
+// 애플리케이션이 시작할 때(리프레시 될 때) 초기화
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+  // console.log(event.target.parentElement);
+  const li = event.target.parentElement;
+  // console.log(li.id);
+  li.remove();
+  // localStorage 의 removeItem 을 쓰지 않고 filter 를 쓰는 이유는 removeItem 은 Array 를 모두 지우기 때문
+  toDos = toDos.filter(todo => todo.id !== parseInt(li.id));
+  saveToDos();
+}
+
 function paintToDo(newToDo) {
   const li = document.createElement('li');
+  li.id = newToDo.id;
   const span = document.createElement('span');
+  span.innerText = newToDo.text;
+  const button = document.createElement('button');
+  button.innerText = '✖';
+
+  button.addEventListener('click', deleteToDo);
+
   li.appendChild(span);
-  span.innerText = newToDo;
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
@@ -14,9 +39,25 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   // console.log(toDoInput.value);
   const newToDo = toDoInput.value;
-  localStorage.setItem('todo-item', newToDo);
   toDoInput.value = '';
-  paintToDo(newToDo);
+  const newToDoObj = {
+    id: Date.now(),
+    text: newToDo,
+  };
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+
+  // 리프레시 되어도 localStorage 에 값이 있으면 값 가져오기
+  toDos = parsedToDos;
+
+  parsedToDos.forEach(paintToDo);
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Visual Studio Code 에서 이모지 사용 단축키
+
+- window 키 + '.'
